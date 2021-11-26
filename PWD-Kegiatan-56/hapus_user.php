a7ceb9f6a30387a12569bcd971e5742c73cf919f
panjisuu <62245442+panjisuu@users.noreply.github.com>
2021-11-26 16:55:10 +0700

    Add files via upload

---
+++ PWD-Kegiatan-56/hapus_user.php
@@ -0,0 +1,7 @@
+<?php
+  include 'config.php';
+  $sql="delete from users where id_user= '$_GET[id]'";
+  mysqli_query($con, $sql);
+    mysqli_close($con);
+  header('location:tampil_user.php');
+?>
